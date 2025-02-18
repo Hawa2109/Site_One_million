770faaa9433f2602c75bc6b89f91ea9953b25601
hawa <hawakone831@gmail.com>
2025-02-18 13:00:47 +0100

    amélioration du forme

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,10 @@
 // alert("Coucou, je teste mon script JS");
 // Pour le hover des card
-document.addEventListener("DOMContentLoaded", function () {
-  let card = document.querySelector(".carte");
-
+let card = document.querySelector(".carte");
+card.document.addEventListener("mouseover", function () {
   card.addEventListener("mouseenter", function () {
     card.classList.add("hovered");
   });
-
   card.addEventListener("mouseleave", function () {
     card.classList.remove("hovered");
   });
@@ -21,8 +19,6 @@ let formulaire = document.querySelector("form");
 
 // On récupére notre champ prenom
 let inputPrenom = document.getElementById("prenom");
-// Pour la taille de la valeur  du champ pseudo
-let taillePrenom = valuePrenom.length;
 
 // On récupére notre champ nom 
 let inputNom = document.querySelector("#nom");
@@ -38,14 +34,20 @@ let valeurP = inputPrenom.value.trim();
 let valeurN = inputNom.value.trim();
 let valeurE = inputEmail.value.trim();
 
+// Pour la taille de la valeur  du champ pseudo
+let taillePrenom = valeurP.length;
+
 // On créer un regex pour notre champ email
 let regexMail = /^[a-z0-9.%+-]+@[a-z0-9.-]+\.[a-z]{2,4}a$/;
 
 // ***************************** PRENOM ***********************
-if (taillePrenomPrenom < 2 || taillePrenomPrenom > 10) {
-  inputPrenom.classList.add("valid");
+if (taillePrenom < 2 || taillePrenom > 10) {
+  // inputPrenom.classList.add("valid");
+  document.querySelector("#prenomError").classList.add("error");
+  document.querySelector("#prenomError").classList.remove("d-none");
 } else {
-  inputPrenom.classList.add("invalid");
+  // inputPrenom.classList.add("invalid");
+  document.querySelector("#prenomError").classList.add("d-none");
 }
 
 
